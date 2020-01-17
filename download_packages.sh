c9ac618a108fd739ff0de64d3db88656816d84ac
victor-s-santos <victor.s.silva@unesp.br>
2020-01-17 20:15:13 -0300

    the .sh file read the .txt file to get the pages

--- download_packages.sh
+++ download_packages.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+N=0
 input="links.txt"
 while IFS= read -r line
 do
-  wget "$line"
+  echo "$line"
+  N=$((N + 1))
+  if [ $N == 10 ]
+  then 
+    sleep 20
+    N=0
+  fi
 done < "$input"
