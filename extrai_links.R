5d9a41f8d84fa997d0d3615756aebe6fb2df1b50
victor-s-santos <victor.s.silva@unesp.br>
2020-01-17 19:46:30 -0300

    refactoring

--- extrai_links.R
+++ extrai_links.R
@@ -9,6 +9,5 @@ webpage <- html_attr(webpage, "href")
 #this way, I can extract all the package links contained in 
 #https://www.bioconductor.org/packages/release/bioc/
 webpage
-write.table(webpage,  "links.txt", append = FALSE, quote = TRUE, sep = " ", eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = FALSE, qmethod = c("escape", "double"))
-
+write.table(webpage,  "links.txt", append = FALSE, quote = FALSE, sep = " ", eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = FALSE, qmethod = c("escape", "double"))
 
