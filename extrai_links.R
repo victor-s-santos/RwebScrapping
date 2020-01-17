6dd1b2b1b2826d3ce2098d98c14bd3e8928aef10
victor-s-santos <victor.s.silva@unesp.br>
2020-01-17 19:34:30 -0300

    export links.txt

--- extrai_links.R
+++ extrai_links.R
@@ -1,5 +1,6 @@
 library(rvest)
 library(httr)
+
 url <- 'links.html'
 webpage <- read_html(url)
 webpage <- html_nodes(webpage, '.dataTable')
@@ -8,4 +9,6 @@ webpage <- html_attr(webpage, "href")
 #this way, I can extract all the package links contained in 
 #https://www.bioconductor.org/packages/release/bioc/
 webpage
+write.table(webpage,  "links.txt", append = FALSE, quote = TRUE, sep = " ", eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = FALSE, qmethod = c("escape", "double"))
+
 
