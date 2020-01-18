0fdb209c74233ae6b674434515ec138e9f281699
victor-s-santos <victor.s.silva@unesp.br>
2020-01-17 21:05:42 -0300

    wget from txt

--- download_packages.sh
+++ download_packages.sh
@@ -3,11 +3,11 @@ N=0
 input="links.txt"
 while IFS= read -r line
 do
-  echo "$line"
+  wget "$line"
   N=$((N + 1))
   if [ $N == 10 ]
   then 
-    sleep 20
+    sleep 10
     N=0
   fi
 done < "$input"
