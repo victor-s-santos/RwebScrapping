de40eefede64138ef5d0398e1d7bd72009a922b1
victor-s-santos <victor.s.silva@unesp.br>
2020-01-28 19:55:46 -0300

    refactored using pipe operator

--- extrai_links.R
+++ extrai_links.R
@@ -2,10 +2,10 @@ library(rvest)
 library(httr)
 
 url <- 'links2.html'
-webpage <- read_html(url)
-webpage <- html_nodes(webpage, 'table')
-webpage <- html_nodes(webpage, 'a')
-webpage <- html_attr(webpage, "href")
+webpage <- read_html(url) %>%
+  html_nodes(webpage, 'table') %>%
+  html_nodes(webpage, 'a') %>%
+  html_attr(webpage, "href")
 webpage
 write.table(webpage,  "links2.txt", append = FALSE, quote = FALSE, sep = " ", eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = FALSE, qmethod = c("escape", "double"))
 
