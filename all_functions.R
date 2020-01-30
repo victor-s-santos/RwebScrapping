be0f4c4339e2fc3a57ecace9001d186e13e1caf6
victor-s-santos <victor.s.silva@unesp.br>
2020-01-30 19:56:57 -0300

    refactoring extract_links function

--- all_functions.R
+++ all_functions.R
@@ -38,13 +38,12 @@ scrap_function <- function(argumento){
 
 
 extract_links <- function(links_html){
-  webpage <- read_html(links_html)
-  webpage <- html_nodes(webpage, '#PageContent')
-  webpage <- html_nodes(webpage, 'table')
-  webpage <- html_nodes(webpage, 'a')
-  webpage <- html_attr(webpage, "href")
-  webpage
-  return (write.table(webpage,  "links2.txt", append = FALSE, quote = FALSE, sep = " ", eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = FALSE, qmethod = c("escape", "double")))
+  webpage <- read_html(links_html) %>%
+    rvest::html_nodes('#PageContent') %>%
+    rvest::html_nodes('table') %>%
+    rvest::html_nodes('a') %>%
+    rvest::html_attr("href")
+  return(write.table(webpage,  "links2.txt", append = FALSE, quote = FALSE, sep = " ", eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = FALSE, qmethod = c("escape", "double")))
 }
 
 
