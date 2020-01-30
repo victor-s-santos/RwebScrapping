7c337c15e015e9c1348052e11ab60c7f44b42b2f
victor-s-santos <victor.s.silva@unesp.br>
2020-01-30 19:18:29 -0300

    refactoring extrai_links.R

--- extrai_links.R
+++ extrai_links.R
@@ -1,12 +1,14 @@
 library(rvest)
 library(httr)
 
+extract_links <- function(links_html){
+  webpage <- read_html(links_html)
+  webpage <- html_nodes(webpage, '#PageContent')
+  webpage <- html_nodes(webpage, 'table')
+  webpage <- html_nodes(webpage, 'a')
+  webpage <- html_attr(webpage, "href")
+  webpage
+  return (write.table(webpage,  "links3.txt", append = FALSE, quote = FALSE, sep = " ", eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = FALSE, qmethod = c("escape", "double")))
+}
 url <- 'links2.html'
-webpage <- read_html(url) %>%
-webpage <- html_nodes(webpage, '#PageContent')
-webpage <- html_nodes(webpage, 'table')
-webpage <- html_nodes(webpage, 'a')
-webpage <- html_attr(webpage, "href")
-webpage
-write.table(webpage,  "links2.txt", append = FALSE, quote = FALSE, sep = " ", eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = FALSE, qmethod = c("escape", "double"))
-
+extract_links(url)
