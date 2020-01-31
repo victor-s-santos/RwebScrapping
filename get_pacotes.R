ebac20b7502a22dd0ed65c8412dfde8325eb5dbe
victor-s-santos <victor.s.silva@unesp.br>
2020-01-31 11:03:13 -0300

    refactoring get_pacotes.R scrip

--- get_pacotes.R
+++ get_pacotes.R
@@ -1,18 +1,20 @@
 meus_pacotes <- readLines("links2.txt")
-i = 0
-for(links in meus_pacotes){
-  for(a in links){
-    if(i == 10){
-      Sys.sleep(3)
-      i = 0
-      
-      download.file(a, destfile="site.html",method="libcurl")
-    }else{
-      download.file(a, destfile="site.html",method="libcurl")
-      i = i + 1 
+download_packages <- function(linksfile){
+  for(links in meus_pacotes){
+    for(a in links){
+      if(i == 10){
+        link <- gsub("https://www.bioconductor.org/packages/release/bioc/html/", "" ,a)
+        Sys.sleep(3)
+        i = 0
+        
+        download.file(a, destfile=link,method="libcurl")
+      }else{
+        download.file(a, destfile=link,method="libcurl")
+        i = i + 1 
+      }
     }
   }
 }
-
-download.file(meus_pacotes[4], destfile='./site.html',method="libcurl")
+download_packages(meus_pacotes)
+#download.file(meus_pacotes[4], destfile='./site.html',method="libcurl")
 #I must think about a way to concatenate the pacage name from the website with the current location
