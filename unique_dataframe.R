c83830fd57b55725abf0280b7c674ad392b3ba14
victor-s-santos <victor.s.silva@unesp.br>
2020-02-02 19:22:47 -0300

    using trycatch to avoid an incorrect append to my list

--- unique_dataframe.R
+++ unique_dataframe.R
@@ -1,18 +1,11 @@
 source("all_functions.R")
 meus_pacotes <- readLines("links2.txt")
-newlist2 <- list()
+newlist3 <- list()
 for(i in 1:length(meus_pacotes)){
-  newlist3[[i]] <- try(scrap_function(meus_pacotes[i]))
-  print(paste(meus_pacotes[i],"Success"))
-  if(inherits(newlist3[[i]], "Anything")){
-    next
-  }
+  newlist3[[i]] <- tryCatch({
+    scrap_function(meus_pacotes[i])}, 
+    error = function(e){})
 }
-length(newlist2)
-print(newlist2[89])
-print(newlist2[1364])
-print(newlist2[1477])
-print(newlist2[1521])
-#these are the broken packages
-#save(newlist2, file = "list_of_all_df.RData")
-unique_dataframe <- bind_rows(newlist2)
+unique_dataframe <- bind_rows(newlist3)
+save(unique_dataframe, file = "unique_df.RData")
+
