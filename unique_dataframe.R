6bc3c65af29bc56a1c09c3fd473daabdea8d687f
victor-s-santos <victor.s.silva@unesp.br>
2020-02-02 00:16:40 -0300

    identifying broken packages

--- unique_dataframe.R
+++ unique_dataframe.R
@@ -2,11 +2,17 @@ source("all_functions.R")
 meus_pacotes <- readLines("links2.txt")
 newlist2 <- list()
 for(i in 1:length(meus_pacotes)){
-  newlist2[[i]] <- try(scrap_function(meus_pacotes[i]))
+  newlist3[[i]] <- try(scrap_function(meus_pacotes[i]))
   print(paste(meus_pacotes[i],"Success"))
-  if(inherits(newlist2[[i]], "Anything")){
+  if(inherits(newlist3[[i]], "Anything")){
     next
   }
 }
 length(newlist2)
-save(newlist2, file = "list_of_all_df.RData")
+print(newlist2[89])
+print(newlist2[1364])
+print(newlist2[1477])
+print(newlist2[1521])
+#these are the broken packages
+#save(newlist2, file = "list_of_all_df.RData")
+unique_dataframe <- bind_rows(newlist2)
